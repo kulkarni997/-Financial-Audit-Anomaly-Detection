3f63569ca13649a433ab97a81c7f5bc52a9a6c34
Srushti Kulkarni <kulkarnisrushti101@gmail.com>
2026-04-01 01:00:07 +0530

    fixed static files with whitenoise

--- audit_system/settings.py
+++ audit_system/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 # ================================
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,6 +115,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'audit', 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ================================
 # MEDIA FILES
