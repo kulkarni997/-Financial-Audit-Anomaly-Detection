4492b06f7fe08a0eff8d68fd6b9de71387fbe549
Srushti Kulkarni <kulkarnisrushti101@gmail.com>
2026-04-01 08:27:37 +0530

    added staticfiles

--- audit_system/settings.py
+++ audit_system/settings.py
@@ -110,10 +110,10 @@ USE_TZ = True
 # ================================
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'audit', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'audit','static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
